be4dbae8ed2cf565793df2239179f5f9f13af586
tetsaiga86 <tetsaiga86@gmail.com>
2016-05-29 19:34:09 -0700

    asdf

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 var firebase = require('firebase');
 var http = require('http');
+var fs = require('fs');
 
 var url = 'https://secure.prontocycleshare.com/data/stations.json';
 
 var credentialBase64 = process.env['FIREBASE_CREDENTIAL'];
 var credentialString = Buffer.from(credentialBase64, 'base64');
-var credential = JSON.parse(credentialString);
 
-firebase.initializeApp({
-  serviceAccount: 'dummy.json',
-  databaseAuthVariableOverride: credential,
-  databaseURL: 'https://bike-map-fd305.firebaseio.com/',
-});
+fs.writeFile('super_secret.json', credentialString, function(err) {
+  if(err) {
+    return console.log(err);
+  }
 
-var db = firebase.database();
-var historicalData = db.ref('data');
-
-setInterval(function(){
-  http.get(url, function(res){
-    var body = '';
-
-    res.on('data', function(chunk){
-      body += chunk;
-    });
-
-    res.on('end', function(){
-      var record = {};
-      var recordBody = JSON.parse(body);
-      record[recordBody.timestamp] = recordBody;
-      historicalData.update(record);
-    });
+  firebase.initializeApp({
+    serviceAccount: 'super_secret.json',
+    databaseURL: 'https://bike-map-fd305.firebaseio.com/',
   });
-}, 1000 * 5);
+
+  var db = firebase.database();
+  var historicalData = db.ref('data');
+
+  setInterval(function(){
+    http.get(url, function(res){
+      var body = '';
+
+      res.on('data', function(chunk){
+        body += chunk;
+      });
+
+      res.on('end', function(){
+        var record = {};
+        var recordBody = JSON.parse(body);
+        record[recordBody.timestamp] = recordBody;
+        historicalData.update(record);
+      });
+    });
+  }, 1000 * 5);
+});
