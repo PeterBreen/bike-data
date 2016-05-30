04f767d521b5795db756a506158045ace53be93f
tetsaiga86 <tetsaiga86@gmail.com>
2016-05-29 20:49:03 -0700

    final corrections for db upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// Imports.  See package.json dependencies,
 var firebase = require('firebase');
 var fs = require('fs');
 var request = require('request');
 
+// External API URL endpoint
 var url = 'https://secure.prontocycleshare.com/data/stations.json';
 
+// Dump the secrets from ENV variable into a file.
+// This is because firebase cannot initialize without secrets stored in a file.
 var credentialBase64 = process.env['FIREBASE_CREDENTIAL'];
 var buffer = new Buffer(credentialBase64, 'base64');
 var credentialString = buffer.toString('ascii');
@@ -13,30 +17,36 @@ fs.writeFile('super_secret.json', credentialString, function(err) {
     return console.log(err);
   }
 
+  // Initialize Firebase server auth.
+  // See https://firebase.google.com/docs/server/setup
   firebase.initializeApp({
     serviceAccount: 'super_secret.json',
     databaseURL: 'https://bike-map-fd305.firebaseio.com/',
   });
 
+  // Get reference to the part of the DB we want to access
   var db = firebase.database();
   var historicalData = db.ref('data');
 
   setInterval(function(){
+    // Get the API data periodically
     request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log("Got API DATA!!!!")
+        /*
+         *  Turn { "timestamp": 12345, "ba": 1, "bx": 2, ... }
+         *  into: { 12345: { "timestamp": 12345, "ba": 1, "bx": 2, ...} }
+         */
         var data = JSON.parse(body);
         var record = {};
+
         record[data.timestamp] = data;
         historicalData.update(record);
-        console.log('sent to db')
       }
     });
-  }, 1000 * 5);
+  }, 1000 * 60 * 5);
 });
 
-
-// for health check
+// for health check.  Otherwise, heroku kills the app.
 var requestProxy = require('express-request-proxy'),
   express = require('express'),
   port = process.env.PORT || 3000,
